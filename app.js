4c0a6d0562a4e04d1adc929a1b220d149f7403d7
Gerges Hany <gergeshany179@gmail.com>
2024-02-03 02:38:46 +0200

    add get method {/api/v1/tours}

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ massege: 'Hello from the server side!', app: 'Natours' });
-});
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.post('/', (req, res) => {
-  res.status(200).json({ massege: 'This is a post request', app: 'Natours' });
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 app.listen(port, () => {
