c104002f450930e0413314b25dfa15c2ac52890b
Gerges Hany <gergeshany179@gmail.com>
2024-02-04 04:53:47 +0200

    add post method {/api/v1/tours}

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const express = require('express');
 const app = express();
 
 const port = 3000;
+app.use(express.json());
+/*
+  app.use(express.json()); 
+  This middleware is used to parse the incoming request body, which is in JSON format, into a JavaScript object before it is passed to the route handler. 
+
+  without this middleware, the req.body object would be undefined, and we would not be able to access the data sent in the request body.
+*/
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -16,6 +23,26 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      console.log(JSON.stringify(newTour));
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(port, () => {
