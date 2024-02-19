607ad327746af918c47c7e80b9e785d9dd563de5
Gerges Hany <gergeshany179@gmail.com>
2024-02-19 22:59:54 +0200

    add (Email Templates with Pug Welcome Emails, Sending Password Reset Emails)

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-const sendEmail = require('../utils/email');
+const Email = require('../utils/email');
 const { use } = require('../app');
 
 const signToken = (id) => {
@@ -40,15 +40,10 @@ const createSendToken = (user, statusCode, res) => {
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm,
-    role: req.body.role,
-  });
+  const newUser = await User.create(req.body);
 
-  console.log(req.body.name, req.body.email, req.body.password, req.body.passwordConfirm, req.body.role);
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, res);
 });
@@ -177,17 +172,10 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   // 3) Send it to user's email
-  const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
-
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
 
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 min)',
-      message,
-    });
-
+    const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
+    await new Email(user, resetURL).sendPasswordReset();
     res.status(200).json({ status: 'success', message: 'Token sent to email!' });
   } catch (err) {
     user.passwordResetToken = undefined;
